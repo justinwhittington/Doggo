25c5ba33bb49d73a5a37df9b6036ba3405c34f9e
Justin <jwhittington92@yahoo.com>
2019-03-03 06:06:38 -0500

    Updates

---
+++ precache-manifest.236e1bb1095ccc7a64a0c2028f00075c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e3c8a54e540cccdc034",
+    "url": "/Doggo/static/css/main.e20098a4.chunk.css"
+  },
+  {
+    "revision": "4e3c8a54e540cccdc034",
+    "url": "/Doggo/static/js/main.4e3c8a54.chunk.js"
+  },
+  {
+    "revision": "6f9ea1b65bad5f427f73",
+    "url": "/Doggo/static/js/1.6f9ea1b6.chunk.js"
+  },
+  {
+    "revision": "29eab2634f35652fa972",
+    "url": "/Doggo/static/js/runtime~main.29eab263.js"
+  },
+  {
+    "revision": "2e605cbb2bbeba5e4d7fa6315dbfcf38",
+    "url": "/Doggo/static/media/dog1.2e605cbb.jpg"
+  },
+  {
+    "revision": "0eadbac9a6eae6d0210770d3d80bedf2",
+    "url": "/Doggo/static/media/dog2.0eadbac9.jpg"
+  },
+  {
+    "revision": "830008d56fa8bfaea8078ce5282d8294",
+    "url": "/Doggo/static/media/dog3.830008d5.jpg"
+  },
+  {
+    "revision": "77bc54c0aee60244c01742dd4ba57866",
+    "url": "/Doggo/index.html"
+  }
+];
